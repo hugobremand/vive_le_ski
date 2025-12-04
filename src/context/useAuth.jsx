56bb057d0865acda103760d918441a9b4d6a5637
hugo <bremand.hugo25@gmail.com>
2025-12-04 09:29:02 +0100

    modif login

---
+++ src/context/useAuth.jsx
@@ -0,0 +1,7 @@
+// useAuth.js
+import { useContext } from "react";
+import { AuthContext } from "./authContext";
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,30 +1,24 @@
 import { useState } from "react";
-import { databases } from "../appwrite";
-import { Query } from "appwrite";
+import { useAuth } from "../context/useAuth";
 
 export default function Login() {
+  const { login } = useAuth(); 
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const handleLogin = async (e) => {
     e.preventDefault();
 
-    try {
-      const response = await databases.listDocuments(
-        "6930103b002c203b18b6",
-        "user",
-        [Query.equal("mail", email), Query.equal("password", password)]
-      );
+    const result = await login(email, password);
 
-      if (response.documents.length > 0) {
-        alert("Connexion réussie !");
-      } else {
-        alert("Email ou mot de passe incorrect !");
-      }
-    } catch (error) {
-      console.error(error);
-      alert("Erreur lors de la connexion");
+    if (!result.success) {
+      alert(result.message);
+      return;
     }
+
+    alert("Connexion réussie !");
+    console.log("Utilisateur :", result.user);
   };
 
   return (
@@ -33,6 +27,7 @@ export default function Login() {
         <h1 className="text-3xl font-bold mb-6 text-center text-blue-700">
           Se connecter
         </h1>
+
         <form onSubmit={handleLogin} className="space-y-5">
           <div>
             <label className="block mb-1 font-medium text-gray-700">
@@ -46,6 +41,7 @@ export default function Login() {
               className="w-full border border-gray-300 px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
             />
           </div>
+
           <div>
             <label className="block mb-1 font-medium text-gray-700">
               Mot de passe
@@ -58,6 +54,7 @@ export default function Login() {
               className="w-full border border-gray-300 px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
             />
           </div>
+
           <button
             type="submit"
             className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition duration-200"
