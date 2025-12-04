d70bd963f6145f16aefdeaa82075055968fca41c
hugo <bremand.hugo25@gmail.com>
2025-12-04 10:01:17 +0100

    modif login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -57,7 +57,7 @@ export default function Login() {
 
           <button
             type="submit"
-            className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition duration-200"
+            className="w-full border border-gray-300 px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
           >
             Se connecter
           </button>
